6bf3b177811f64975cb6e2736651cdda46a0b064
Nurbek Imangazin <nurbekimangazin@gmail.com>
2025-04-17 16:26:31 -0400

    test

--- syllabus_api.py
+++ syllabus_api.py
@@ -74,18 +74,17 @@ def exempt():
     year, term, department = extract_info(course_code)
     orgUnitId = csv_db.get_orgUnitId_by_code(course_code)
     print(f'OrgUnitId: {orgUnitId}')
-    # Update the DB, mark the course as exempted by changing Recorded field value to 2.
-    if(orgUnitId is not None):
-        exempt_df = pd.DataFrame([{'OrgUnitId': int(orgUnitId)}])
-        csv_db.update_syllabus_recorded(exempt_df, 2)
+    # # Update the DB, mark the course as exempted by changing Recorded field value to 2.
+    # exempt_df = pd.DataFrame([{'OrgUnitId': int(orgUnitId)}])
+    # csv_db.update_syllabus_recorded(exempt_df, 2)
 
-        # Recreate the html file
-        department_courses_df = csv_db.get_department_cources(term, year, department)
-        d2l_functions.generate_syllabus_html(department_courses_df, 'downloads')
+    # # Recreate the html file
+    # department_courses_df = csv_db.get_department_cources(term, year, department)
+    # d2l_functions.generate_syllabus_html(department_courses_df, 'downloads')
 
-        #Upload it to BS
-        access_token = get_access_token()
-        d2l_functions.upload_content_html(department_courses_df, year, term, access_token)
+    # #Upload it to BS
+    # access_token = get_access_token()
+    # d2l_functions.upload_content_html(department_courses_df, year, term, access_token)
 
     return jsonify({"status": "success", "message": f"{course_code} has been exempted. OrgUnitID={orgUnitId}"}), 200
 
